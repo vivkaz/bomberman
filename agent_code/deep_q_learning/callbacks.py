ad1ee7db74f750e547ebd1b5d53e4046c6b225d6
josef <joseftremmel@gmx.de>
2022-03-11 16:33:50 +0100

    agent with implementation of deep learning 2

---
+++ agent_code/deep_q_learning/callbacks.py
@@ -0,0 +1,133 @@
+import os
+import pickle
+import random
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from collections import deque
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+
+
+
+
+def setup(self):
+    """
+    Setup your code. This is called once when loading each agent.
+    Make sure that you prepare everything such that act(...) can be called.
+
+    When in training mode, the separate `setup_training` in train.py is called
+    after this method. This separation allows you to share your trained agent
+    with other students, without revealing your training code.
+
+    In this example, our model is a set of probabilities over actions
+    that are is independent of the game state.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+    # initialize the model and save as self.model_train
+    # if mode is train the model self.model_train becomes self.model
+
+    # number of outputs of the model
+    #self.n_outputs = 5
+    #self.inputs_shape = (9, 9, 2)
+    """
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv2D(8, 4, activation="elu", padding='same', input_shape=self.inputs_shape),
+        tf.keras.layers.MaxPooling2D(2),
+        tf.keras.layers.Conv2D(4, 3, activation="elu", padding='same'),
+        tf.keras.layers.MaxPooling2D(2),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(10, activation="relu"),
+        tf.keras.layers.Dense(10, activation="relu"),
+        tf.keras.layers.Dense(self.n_outputs, activation="softmax")
+
+    ])
+    """
+    #name of loaded model either new initialize or pretrained model
+    load_model = "initialize_model"
+    #load_model = "saved_model_without_coin_9x9"
+
+
+    try:
+        self.model = tf.keras.models.load_model(load_model)
+    except:
+        self.logger.debug("model cant be loaded from save place")
+    self.n_outputs = self.model.get_config()['layers'][-1]["config"]["units"]
+
+def act(self, game_state: dict) -> str:
+    """
+    Your agent should parse the input, think, and take a decision.
+    When not in training mode, the maximum execution time for this method is 0.5s.
+
+    :param self: The same object that is passed to all of your callbacks.
+    :param game_state: The dictionary that describes everything on the board.
+    :return: The action to take as a string.
+    """
+
+    #epsilon is a hyperparameter that introduces a random factor for the decisoin process for the first trained rounds. This supports the agent to discover the enviroment
+    epsilon = max(1 - game_state["round"] / 250, 0.01)
+
+    #inputs are the input data for the network, which calculates an action based on the inputs. The function state_to_features transform the game states in to a 17x17xn matrix where the
+    #information abount position, walls, coins, ... are stored in the 3rd dimension
+    inputs = state_to_features(game_state)
+    # todo Exploration vs exploitation
+
+    self.logger.debug("Querying model for action.")
+
+    #actual decision process
+    #the action with the highes Q-value is choosen
+    if self.train:
+        if np.random.rand() < epsilon:
+            decision =  np.random.randint(5)
+        else:
+            Q_values = self.model.predict(inputs[np.newaxis])
+            decision =  np.argmax(Q_values[0])
+    else:
+        decision = np.argmax(self.model.predict(inputs[np.newaxis]))
+
+
+    self.logger.info(f"Action {ACTIONS[decision]} at step {game_state['step']}")
+
+    return ACTIONS[decision]
+
+
+def state_to_features(game_state: dict) -> np.array:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Converts the game state to the input of your model, i.e.
+    a feature vector.
+
+    You can find out about the state of the game environment via game_state,
+    which is a dictionary. Consult 'get_state_for_agent' in environment.py to see
+    what it contains.
+
+    :param game_state:  A dictionary describing the current game board.
+    :return: np.array
+    """
+    # This is the dict before the game begins and after it ends
+    if game_state is None:
+        return None
+
+
+    field = game_state["field"]
+    agents_position = game_state["self"][3]
+    coin_position = game_state["coins"]
+
+    #implement the position of the coins
+    x_coin = []
+    y_coin = []
+    for i in coin_position:
+        x_coin.append(i[0])
+        y_coin.append(i[1])
+    x_coin = np.array(x_coin)
+    y_coin = np.array(y_coin)
+
+    #putting it all into the matrix inputs
+    inputs = np.zeros(field.shape + (2,))
+    inputs[:, :, 0] = field
+    inputs[:, :, 1][agents_position[0], agents_position[1]] = 1
+    #inputs[:, :, 2][x_coin,y_coin] = 1
+
+    return inputs
---
+++ agent_code/deep_q_learning/train.py
@@ -0,0 +1,181 @@
+from collections import namedtuple, deque
+import tensorflow as tf
+import pickle
+from typing import List
+import numpy as np
+import events as e
+from .callbacks import state_to_features
+import matplotlib.pyplot as plt
+from datetime import datetime
+
+
+
+ACTIONS = ['UP', 'RIGHT', 'DOWN', 'LEFT', 'WAIT', 'BOMB']
+
+#function to give an action like "WAIT" a specific number
+def transform_actions_to_number(action):
+    result = []
+    for act in action:
+        result.append(ACTIONS.index(act))
+    return np.array(result)
+
+#function "sample_experiences" samples a random set of experiences. A experience is the information about the state the action the reward and the next state of one step in the game.
+#the parameter batch_size defines the number of experiences that are use for one training iteration
+def sample_experiences(self, batch_size):
+    indices = np.random.randint(len(self.replay_memory), size=batch_size)
+    batch = [self.replay_memory[index] for index in indices]
+    states, actions, rewards, next_states = [np.array([experience[field_index] for experience in batch])
+                                                    for field_index in range(4)]
+    return states, actions, rewards, next_states
+
+def setup_training(self):
+    """
+    Initialise self for training purpose.
+
+    This is called after `setup` in callbacks.py.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    """
+
+    #variable to buffer the experiences of previous setps
+    self.replay_memory = deque(maxlen=2000)
+
+    #variavle to save the rewards per step
+    self.rewards = []
+
+
+def game_events_occurred(self, old_game_state: dict, self_action: str, new_game_state: dict, events: List[str]):
+
+    """
+    Called once per step to allow intermediate rewards based on game events.
+
+    When this method is called, self.events will contain a list of all game
+    events relevant to your agent that occurred during the previous step. Consult
+    settings.py to see what events are tracked. You can hand out rewards to your
+    agent based on these events and your knowledge of the (new) game state.
+
+    This is *one* of the places where you could update your agent.
+
+    :param self: This object is passed to all callbacks and you can set arbitrary values.
+    :param old_game_state: The state that was passed to the last call of `act`.
+    :param self_action: The action that you took.
+    :param new_game_state: The state the agent is in now.
+    :param events: The events that occurred when going from  `old_game_state` to `new_game_state`
+    """
+    self.logger.debug(f'Encountered game event(s) {", ".join(map(repr, events))} in step {new_game_state["step"]}')
+
+    #function "buffer_information" saves the experience of one step in the variable self.replay_memory starts a step 2 since in setp 1 the old_game_state is none
+    def buffer_information():
+        if new_game_state["step"] >=2:
+            self.replay_memory.append((state_to_features(old_game_state),self_action,reward_from_events(self,events),state_to_features(new_game_state)))
+
+
+    buffer_information()
+
+
+
+def end_of_round(self, last_game_state: dict, last_action: str, events: List[str]):
+    """
+    Called at the end of each game or when the agent died to hand out final rewards.
+    This replaces game_events_occurred in this round.
+
+    This is similar to game_events_occurred. self.events will contain all events that
+    occurred during your agent's final step.
+
+    This is *one* of the places where you could update your agent.
+    This is also a good place to store an agent that you updated.
+
+    :param self: The same object that is passed to all of your callbacks.
+    """
+
+    batch_size = 32#number of old experience used for updating the model
+    discount_factor = 0.95#value which weights
+    optimizer = tf.keras.optimizers.Adam(learning_rate=1e-3)
+    loss_fn = tf.keras.losses.mean_squared_error
+
+    if last_game_state["round"] >= 1:
+        experiences = sample_experiences(self, batch_size)
+        states, actions, rewards, next_states = experiences
+        states = np.array(states)
+        next_states = np.array(next_states)
+
+        with open("states.npy", 'wb') as f:
+            np.save(f, states, allow_pickle=True)
+        next_Q_values = self.model.predict(next_states)
+        max_next_Q_values = np.max(next_Q_values, axis=1)
+        target_Q_values = (rewards + (1) * discount_factor * max_next_Q_values)
+        target_Q_values = target_Q_values.reshape(-1, 1)
+        # print("actions : ",actions)
+        actions = transform_actions_to_number(actions)
+        # print("actions : ", actions)
+        mask = tf.one_hot(actions, self.n_outputs)
+        with tf.GradientTape() as tape:
+            # print("states",states)
+            # print(self.model(states))
+            all_Q_values = self.model(states)
+            Q_values = tf.reduce_sum(all_Q_values * mask, axis=1, keepdims=True)
+            loss = tf.reduce_mean(loss_fn(target_Q_values, Q_values))
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        #print("grads: ",grads)
+        optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+        self.logger.info("update model")
+
+    #save model
+    self.model.save("saved_model")
+
+
+    self.logger.debug(f'Encountered event(s) {", ".join(map(repr, events))} in final step')
+
+
+    # Store the model
+    #with open("my-saved-model.pt", "wb") as file:
+    #    pickle.dump(self.model, file)
+    sum = 0
+    for i in range(len(self.replay_memory)):
+        sum += self.replay_memory[i][2]
+    #self.rewards.append(np.sum(self.replay_memory[3]))
+    self.replay_memory.clear()
+    self.rewards.append(sum)
+    self.logger.info(f'end of round {last_game_state["round"]} with total reward : {sum}')
+
+    if last_game_state["round"] ==1:
+        now = datetime.now()
+        self.date_time = now.strftime("%m_%d_%Y_%H_%M_%S")
+    with open(f"rewards_{self.date_time}.txt",'a') as f:
+        f.write(f'{sum}')
+        f.write('\n')
+
+    #reset protokoll file in first round
+    if last_game_state["round"] == 1:
+        with open("training_protokoll.txt",'r+') as f:
+            f.truncate(0)
+
+    #write data to protokoll such that the training status can be supervised during training
+    with open("training_protokoll.txt",'a') as f:
+        f.write(f"[{datetime.now().strftime('%H_%M_%S')}] finished epoche {last_game_state['round']} with total reward {sum}")
+        f.write("\n")
+
+def reward_from_events(self, events: List[str]) -> int:
+    """
+    *This is not a required function, but an idea to structure your code.*
+
+    Here you can modify the rewards your agent get so as to en/discourage
+    certain behavior.
+    """
+    game_rewards = {
+        e.INVALID_ACTION: -20,
+        e.MOVED_UP:10,
+        e.MOVED_DOWN: 10,
+        e.MOVED_LEFT: 10,
+        e.WAITED: 0,
+        e.MOVED_RIGHT: 10,
+        #e.COIN_COLLECTED: 20
+
+    }
+    reward_sum = 0
+    for event in events:
+        if event in game_rewards:
+            reward_sum += game_rewards[event]
+    self.logger.info(f"Awarded {reward_sum} for events {', '.join(events)}")
+    return reward_sum
